b8f89cf32bfb4d0ab39fbcf859e4be27f975af1f
raziqtechnologies <81863529+raziqtechnologies@users.noreply.github.com>
2024-10-09 15:51:47 +0100

    some files

--- a.py
+++ a.py
@@ -1,2 +1,10 @@
-# Greeting Program
-print("Welcome to my Python program!")
+import time
+
+# Print "Hello, World!"
+print("Hello, World!")
+
+# Wait for 10 seconds
+time.sleep(10)
+
+# Print the second message
+print("And I waited for 10 secs.")
