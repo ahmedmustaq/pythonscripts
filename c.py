5a32d65f629ccbdd1c41eb857bb3f75ce5926fae
ahmedmustaq <130282587+ahmedmustaq@users.noreply.github.com>
2024-11-12 19:56:18 +0000

    Update c.py

--- c.py
+++ c.py
@@ -1,2 +1,13 @@
-# Simple Message Program
-print("Python is fun!")
+import os
+import time
+
+# Access environment variables
+customer = os.getenv("CUSTOMER", "default_customer")  # Default value if CUSTOMER is not set
+image = os.getenv("IMAGE", "default_image")          # Default value if IMAGE is not set
+email = os.getenv("CUSTOMER_EMAIL", "a@b.com")          # Default value if IMAGE is not set
+
+# Print "Hello, World!"
+print("Hello, World!")
+
+# Print the second message with environment variables
+print(f"And I waited for 10 secs. Environment variables: CUSTOMER={customer}, IMAGE={image},CUSTOMER_EMAIL={email}")
